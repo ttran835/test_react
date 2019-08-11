f2ef4cf6c146b1271aa51939598ae3d0a61a6569
ttran835 <timothy.tran835@gmail.com>
2019-08-11 16:48:49 -0700

    second commit to update react component information. Next item to complete is to install appropriate redux and react router information to build a mock application. May integrate GraphQL

--- client/src/Components/App.jsx
+++ client/src/Components/App.jsx
@@ -1,16 +1,42 @@
 import React, { Component } from 'react';
 import Axios from 'axios';
 
+/* component */
+
+import NavBar from './NavBar/NavBar';
+import Hero from './Hero/Hero';
+
 export default class App extends Component {
   constructor(props) {
-    super(props)
+    super(props);
 
     this.state = {};
   }
 
+  // to grab data
+  componentDidMount() {}
+
   render() {
     return (
-      <div>Hello</div>
-    )
+      <div className="main-wrapper">
+        <nav className="navbar-expand-lg navbar-light bg-light">
+          <a class="navbar-brand" href="#">
+            Navbar
+          </a>
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon" />
+          </button>
+          <NavBar />
+        </nav>
+      </div>
+    );
   }
 }
